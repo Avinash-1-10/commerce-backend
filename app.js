171f9e6a48c367344eaed4290b9b33d5114ca49e
sneta gupta <snetagupta4nov@gmail.com>
2024-07-09 23:31:51 +0530

    Add mongodb

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { configDotenv } from "dotenv";
 import userRouter from "./routes/userRouter.js";
+import connectDB from "./config/db.js";
+import cors from "cors"
+
 configDotenv();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+connectDB()
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
